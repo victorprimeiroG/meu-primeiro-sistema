35267596485e54879e685c57fc3781a747cc9d72
victorprimeiroG <110112719+victorprimeiroG@users.noreply.github.com>
2026-08-13 09:07:06 -0300

    Delete src directory

--- src/br/com/meusistema/model/Carro.java
+++
@@ -1,8 +0,0 @@
-package src.br.com.meusistema.model;
-public class Carro {
-public String modelo;
-public String cor;
-public void buzinar() {
-System.out.println("O " + modelo + " " + cor + " fez: BIIII BIIII!");
-    }
-}
